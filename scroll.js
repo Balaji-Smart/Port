f5acac122d4da90b1f6751cf293971d3737e7d91
Balaji K <smartbalaji050@gmail.com>
2024-04-25 22:30:37 +0530

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,12 @@
+ScrollReveal({
+    distance: '80px',
+    duration: 1000,
+    delay: 200,
+ });
+
+ ScrollReveal().reveal('.home-content, heading', {origin: 'top'});
+ ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin: 'button'});
+ ScrollReveal().reveal('.home-contact h1, .about-img', {origin: 'left'});
+ ScrollReveal().reveal('.home-contact p, .about-content', {origin: 'right'});
+ ScrollReveal().reveal('.editing', {origin: 'left'});
+ ScrollReveal().reveal('.tagline', { delay: 500 });
---
+++ typed.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text',{
+    strings: ['AI Engineer | Software Developer | Editor'],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 3000,
+    loop: true,   
+})
